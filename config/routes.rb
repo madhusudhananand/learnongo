f2a157dec84ce360e4d14aac65f0411cce636009
madhusudhananand <madhusudhan.anand@techolution.com>
2021-03-06 17:03:23 +0530

    static controller pages created for landing and privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
+  root "static_pages#landing_page"
+  # get 'static_pages/landing_page'
   get 'static_pages/privacy_policy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "privacy_policy", to: "static_pages#privacy_policy"
 end
